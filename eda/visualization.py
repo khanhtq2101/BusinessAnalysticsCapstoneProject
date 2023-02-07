78890e1966843baa31eb121b3c1facb24cc1f9c5
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2023-02-07 21:20:03 +0700

    update visualization

--- eda/visualization.py
+++ eda/visualization.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot  as plt
 import pandas as pd
+from pyspark.sql.functions import col
 
 
 def correlation_heatmap(dataset, ordinal_attributes, numeric_attributes, target_col = "Attrition", masked= True):
@@ -19,14 +20,14 @@ def correlation_heatmap(dataset, ordinal_attributes, numeric_attributes, target_
   '''
   corr_heatmap = correlation(dataset, ordinal_attributes, numeric_attributes, target_col)
 
-  mask = np.ones_like(corr_heatmap)
+  mask = np.zeros_like(corr_heatmap)
   if masked:
     for i in range(mask.shape[0]):
         for j in range(mask.shape[1]):
-            if j < i:
-                mask[i, j] = 0
+            if j >= i:
+                mask[i, j] = 1
   fig = plt.figure()
-  fig.set_size_inches(16, 12)
+  fig.set_size_inches(16, 14)
   ax = fig.add_axes([0, 0, 1, 1])
 
   sns.heatmap(corr_heatmap, axes = ax, cmap="YlGnBu", mask= mask, vmin= -0.9, vmax= 0.9, annot= True)
@@ -40,11 +41,12 @@ def correlation_heatmap(dataset, ordinal_attributes, numeric_attributes, target_
   return fig
 
 
-def continous_plot(dataset, continous_attributes, target_name= "Attrition", plot_type= 'boxplot'):
+def continous_plot(dataset, continous_attributes, target_name= "Attrition", plot_type= 'boxplot', grouped= True):
   '''
   Parameter:
     continous_attributes: list of strings, name of attribute
     plot_type: type of plot, suppoted (boxplot, histplot, kdeplot, violinplot)
+    grouped: boolean, whether group by target attribute or not
   Return: 
     figure of plot
   '''
@@ -64,14 +66,32 @@ def continous_plot(dataset, continous_attributes, target_name= "Attrition", plot
     elif plot_type == 'kdeplot':
       sns.kdeplot(data= selected_data, x= att_name, hue= target_name, ax = axes[i // 6, i % 6])
     elif plot_type == 'histplot':
-      sns.histplot(data= selected_data, x= att_name, hue = target_name, 
-                    multiple="dodge", ax = axes[i // 6, i % 6], kde=True)
-      
+      if grouped:
+        sns.histplot(data= selected_data, x= att_name, hue = target_name, 
+                      multiple="dodge", ax = axes[i // 6, i % 6], kde=True)
+      else:
+        sns.histplot(data= selected_data, x= att_name, ax = axes[i // 6, i % 6], kde=True)
+    if plot_type != 'boxplot':
+      if i != 5:
+        axes[i // 6, i % 6].get_legend().remove()
+    if i % 6 != 0:
+      axes[i // 6, i % 6].set_ylabel('')
+
   return fig
 
-def hist_ordinal(dataset, ordinal_attributes, target_name= "Attrition"):
+
+def hist_ordinal(dataset, ordinal_attributes, target_name= "Attrition", attrition_percent= True):
+  '''
+  Parameter:
+    ordinal_attributes: list of strings, name of attribute
+    plot_type: type of plot, suppoted (countplot, histplot))
+    attrition_percent: boolean plot the percentage of attrition line
+  Return: 
+    figure of plot
+  '''
+
   fig, axes = plt.subplots(3, 4)
-  fig.set_size_inches(20, 10)
+  fig.set_size_inches(21, 10)
 
   for (i, att_name) in enumerate(ordinal_attributes):
     selected_data = dataset.select([att_name, target_name]).collect()
@@ -82,23 +102,55 @@ def hist_ordinal(dataset, ordinal_attributes, target_name= "Attrition"):
       sns.countplot(data= selected_data, x= att_name, hue = target_name, ax = axes[i//4, i%4])
     if i != 3:
       axes[i//4, i%4].get_legend().remove()
+    if i % 4 != 0:
+      axes[i//4, i%4].set_ylabel('')
+
+    #plot percentage line
+    if attrition_percent:
+      count = dataset.groupBy([att_name, target_name]).count().sort(att_name)
+      values = np.array(count.select(att_name).distinct().sort(att_name).collect())
+      att_values = [values[i, 0] for i in range(values.shape[0])]
+      count_total = []
+      count_yes = []
+      count_no = []
+
+      for value in att_values:
+        n_yes = count.where((col(target_name) == 'Yes') & (col(att_name) == value)).select('count').collect()[0][0]
+        n_no = count.where((col(target_name) == 'No') & (col(att_name) == value)).select('count').collect()[0][0]
+        count_yes.append(n_yes)
+        count_no.append(n_no)
+        count_total.append(n_yes + n_no)
+
+      yes_per = [(count_yes[i] / count_total[i])*100 for i in range(len(att_values))]
+      
+      twin = axes[i//4, i%4].twinx()
+      twin.plot(list(range(len(yes_per))), yes_per, marker= 'o', color= 'cadetblue', label= "Attrition rate", linewidth= 2, markersize= 8)
+      twin.set_yticks(list(range(0, 101, 20)))
+      if i == 3:
+        twin.legend()
+      if i % 4 == 3:
+        twin.set_ylabel('percentage')
+  axes[2, -1].axis('off')
+
 
   fig.suptitle("Histogram of ordinal variables by attrition", size= 16)
+  
   return fig
 
-def categorical_plot(dataset, categorical_attributes, target_name= "Attrition", plot_type= 'countplot'):
+
+def nominal_plot(dataset, nominal_attributes, target_name= "Attrition", plot_type= 'countplot'):
   '''
   Parameter:
-    cetegorical_attributes: list of strings, name of attribute
-    plot_type: type of plot, suppoted (countplot, histplot))
+    nominal_attributes: list of strings, name of attribute
+    plot_type: type of plot, suppoted (countplot)
   Return: 
     figure of plot
   '''
 
-  fig, axes = plt.subplots(1, len(categorical_attributes))
-  fig.set_size_inches(18, 1 + 3*(len(categorical_attributes) // 5))
+  fig, axes = plt.subplots(1, len(nominal_attributes))
+  fig.set_size_inches(18, 1 + 3*(len(nominal_attributes) // 5))
 
-  for (i, att_name) in enumerate(categorical_attributes):
+  for (i, att_name) in enumerate(nominal_attributes):
     selected_data = dataset.select([att_name, target_name]).collect()
     selected_data = pd.DataFrame(selected_data, columns= [att_name, target_name])
     axes[i].set_xlabel(att_name)
@@ -106,13 +158,51 @@ def categorical_plot(dataset, categorical_attributes, target_name= "Attrition", 
     if plot_type == 'countplot':
       sns.countplot(data= selected_data, x= att_name, hue = target_name, ax = axes[i])
       axes[i].set_xticklabels(axes[i].get_xticklabels(), rotation=45, horizontalalignment='right')
-      if i >=1:
-        axes[i-1].get_legend().remove()
-    elif plot_type == 'histplot':
-      if i == len(categorical_attributes) - 1:
-        sns.histplot(data= selected_data, x= att_name, y = target_name, ax = axes[i], cbar= True)
-      else: 
-        sns.histplot(data= selected_data, x= att_name, y = target_name, ax = axes[i], cbar= False)
-      axes[i].tick_params(axis='x', rotation=90)
+    if i >=1:
+      axes[i-1].get_legend().remove()
+      
+
+  return fig
+
+
+def percentage_stacked(dataset, nominal_attributes, target_name= "Attrition", plot_type= 'countplot'):
+  '''
+  Parameter:
+    nominal_attributes: list of strings, name of attribute
+    plot_type: type of plot, suppoted (countplot, histplot))
+  Return: 
+    figure of plot
+  '''  
+  fig, axes = plt.subplots(1, len(nominal_attributes))
+  fig.set_size_inches(20, 1 + 3*(len(nominal_attributes) // 5))
+
+  for (i, att_name) in enumerate(nominal_attributes):
+    count = dataset.groupBy([att_name, target_name]).count().sort(att_name)
+
+    values = np.array(count.select(att_name).distinct().sort(att_name).collect())
+    att_values = [values[i, 0] for i in range(values.shape[0])]
+    count_yes = []
+    count_no = []
+    count_total = []
+
+    for value in att_values:
+      n_yes = count.where((col(target_name) == 'Yes') & (col(att_name) == value)).select('count').collect()[0][0]
+      n_no = count.where((col(target_name) == 'No') & (col(att_name) == value)).select('count').collect()[0][0]
+      count_yes.append(n_yes)
+      count_no.append(n_no)
+      count_total.append(n_yes + n_no)
+
+    yes_per = [(count_yes[i] / count_total[i])*100 for i in range(len(att_values))]
+    no_per = [100 - yes_per[i] for i in range(len(att_values))]
+    axes[i].bar(att_values, yes_per, label = 'Yes')
+    axes[i].bar(att_values, no_per, bottom= yes_per, label= 'No')
+    axes[i].tick_params(axis='x', rotation=90)
+
+    axes[i].set_xlabel(att_name)
+    axes[i].set_ylabel("Percentage")
+    if i != len(nominal_attributes):
+      axes[i].set_ylabel('')
+
+  axes[-1].legend()
 
   return fig
--- machine_learning/testing.py
+++ machine_learning/testing.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator, BinaryClassificationEvaluator, Evaluator
 
 def evaluateModel(test, mlModel):
@@ -13,8 +13,12 @@ def evaluateModel(test, mlModel):
   y_pred = prediction.select("prediction").collect()
   y_orig = prediction.select("label").collect()
   confusionMatrix = confusion_matrix(y_orig, y_pred)
+  precision = precision_score(y_orig, y_pred, average= None)
+  recall = recall_score(y_orig, y_pred, average= None)
 
   print("Accuracy:", acc)
   print("Confusion matrix:\n", confusionMatrix)
+  print("Precision:", precision)
+  print("Recall:", recall)
 
-  return acc, confusionMatrix
+  return acc, confusionMatrix, precision, recall
